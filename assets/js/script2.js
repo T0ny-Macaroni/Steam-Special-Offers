131de5e1d44d147e31b22061e842afede5df2e74
TonyMacaroni <Tonygiuliani3502@gmail.com>
2023-07-19 09:08:48 -0500

    cleaned code

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,18 +1,18 @@
 function populateDetails() {
-	var savedDetails = JSON.parse(localStorage.getItem('Game Details'));
-	var savedId = JSON.parse(localStorage.getItem('Game ID'));
-	console.log(savedId);
-	console.log(savedDetails);
+    var savedDetails = JSON.parse(localStorage.getItem('Game Details'));
+    var savedId = JSON.parse(localStorage.getItem('Game ID'));
+    console.log(savedId);
+    console.log(savedDetails);
 
-	$(".gameTitle").text(savedDetails.name);
-	$(".description").text(savedDetails.desc);
+    $(".gameTitle").text(savedDetails.name);
+    $(".description").text(savedDetails.desc);
     $(".publish").text('Publisher: ' + savedDetails.dev_details.developer_name);
     $("img").attr('src', savedDetails.images.screenshot[0]);
     $("#gameVideo").attr('src', savedDetails.images.videos[0]);
-    
-	$("#windowsRec").text(savedDetails.sys_req.window.recomm);
-	$("#windowsMin").text(savedDetails.sys_req.window.min);
-    
+
+    $("#windowsRec").text(savedDetails.sys_req.window.recomm);
+    $("#windowsMin").text(savedDetails.sys_req.window.min);
+
     function getTopReview() {
         const reviews = {
             async: true,
@@ -24,31 +24,29 @@ function populateDetails() {
                 'X-RapidAPI-Host': 'games-details.p.rapidapi.com'
             }
         };
-        
+
         $.ajax(reviews).done(function (response) {
-            console.log(response);
             $('#topReview').text(response[0].content);
         });
     }
 
-	// getTopReview();
+    // getTopReview();
     // getFunnyReview();
-function getFunnyReview(){
+    function getFunnyReview() {
 
-    const reviews2 = {
-        async: true,
-        crossDomain: true,
-        url: `https://games-details.p.rapidapi.com/${savedId}/reviews/funny/30`,
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '430c836d45msh0050ea49f6b8455p1f8a07jsn725aabd514c9',
-            'X-RapidAPI-Host': 'games-details.p.rapidapi.com'
-        }
-    };
-    
+        const reviews2 = {
+            async: true,
+            crossDomain: true,
+            url: `https://games-details.p.rapidapi.com/${savedId}/reviews/funny/30`,
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '430c836d45msh0050ea49f6b8455p1f8a07jsn725aabd514c9',
+                'X-RapidAPI-Host': 'games-details.p.rapidapi.com'
+            }
+        };
+
         $.ajax(reviews2).done(function (response) {
-        console.log(response);
-        $('#funnyReview').text(response[0].content);
+            $('#funnyReview').text(response[0].content);
         });
     }
 }
@@ -58,14 +56,11 @@ convertINRtoUSD()
 
 function convertINRtoUSD() {
     const amountString = localStorage.getItem('Game Price1')
-    const amountCleared = amountString.replace( /₹\s*/, "")
-    console.log(typeof amountCleared);
+    const amountCleared = amountString.replace(/₹\s*/, "")
     const amountInteger = Number(amountCleared.replaceAll(',', ''));
-    console.log(amountInteger);
     const exchangeRate = 0.012; // Exchange rate: 1 INR = 0.014 USD
-    const convertedAmount = amountInteger * exchangeRate; 
+    const convertedAmount = amountInteger * exchangeRate;
     let formattedAmount = convertedAmount.toFixed(2)
-    console.log("USD", convertedAmount);
     $('.price').text('$ ' + formattedAmount);
-    
+
 }
